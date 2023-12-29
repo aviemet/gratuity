fb918d6b0d4b93ffc30d47b5f3ca788f5a4e29bd
Avram Walden <aviemet@gmail.com>
2023-12-29 15:04:57 -0800

    feat: more work on serializer generator

--- lib/generators/serializer/serializer_generator.rb
+++ lib/generators/serializer/serializer_generator.rb
@@ -1,10 +1,43 @@
+require 'rails/generators'
+require 'rails/generators/named_base'
+
 class SerializerGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("templates", __dir__)
+  argument :attributes, type: :array, default: [], banner: 'attribute attribute'
 
-  class_option :actions, type: :array, default: []
+  class_option :base_only, type: :boolean, default: false, desc: "Only generate the base serializer"
+  class_option :only, type: :array, default: [], desc: "Generate only the actions specified"
+  class_option :except, type: :array, default: [], desc: "Don't generate the actions specified"
+
+  def validate_options
+    return unless options[:only].present? && options[:except].present?
+
+    raise ArgumentError, "Only one of --only or --except can be used as flags"
+  end
 
   def create_serializer
-    pry
-    # template 'serializer.rb', "app/serializers/#{file_path}_serializer.rb"
+    validate_options
+
+    template 'serializer.rb', "app/serializers/#{file_path}_serializer.rb"
+
+    generate_named_serializers
+  end
+
+  private
+
+  def generate_named_serializers
+    views = [:edit, :form_data, :index, :show]
+
+    if options[:only].present?
+      views.select! { |view| options[:only].include?(view.to_s) }
+    end
+
+    if options[:except].present?
+      views.reject! { |view| options[:except].include?(view.to_s) }
+    end
+
+    views.each do |view|
+      template "#{view}_serializer.rb.erb", "app/serializers/#{file_path.pluralize}_#{view}_serializer.rb"
+    end
   end
 end
